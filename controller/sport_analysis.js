1748c0b0484fc2a61c9382c2ef0873c086125b7d
Cullen <cullen@example.com>
2022-09-22 16:02:41 +0800

    [Feature]Add sport recommend function
    date 20220922

--- controller/sport_analysis.js
+++ controller/sport_analysis.js
@@ -21,4 +21,28 @@ function SportAnalysis (app) {
       return res.json({result: 'ok', ret: ret})
     })
   })
+
+  /**
+   * GET /api/sport_analysis/recommend
+   *
+   * 賽事與玩法推薦
+   */
+  app.get('/api/sport_analysis/recommend', async (req, res) => {
+    const sport = req.query.sport
+    const matchRecommends = req.query.matches
+
+    return sportAnalysisLib.getPlaysportPrediction(sport, (err, oriRet) => {
+      if (err) {
+        return res.json({result: 'error', msg: err.message})
+      }
+
+      return sportAnalysisLib.getRecommend(oriRet, matchRecommends, (err, finalRet) => {
+        if (err) {
+          return res.json({result: 'error', msg: err.message})
+        }
+
+        return res.json({result: 'ok', ret: finalRet})
+      })
+    })
+  })
 }
