275ceb053c9d8165674a6ae6bf093f3443e672e2
Cullen <cullen@example.com>
2022-09-27 11:54:05 +0800

    [Feature]Adjust google sheets connection api
    date 20220927

--- controller/banks.js
+++ controller/banks.js
@@ -6,9 +6,9 @@ const bankLib = require('../lib/banks')
 
 function Banks (app) {
   /**
-   * GET /api/banks/accounts
+   * GET /api/banks
    *
-   * 取得帳戶API
+   * 取得銀行API
    */
   app.get('/api/banks', async (req, res) => {
     try {
