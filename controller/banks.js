edbb150b91484f0e95aa324be26b35f741f166d7
Cullen <cullen@example.com>
2022-09-28 17:48:11 +0800

    [Feature]Add get bank detail api
    case 20220928

--- controller/banks.js
+++ controller/banks.js
@@ -6,11 +6,29 @@ function Banks (app) {
   /**
    * GET /api/banks
    *
-   * 取得銀行API
+   * 取得銀行列表API
    */
   app.get('/api/banks', async (req, res) => {
     try {
       const ret = await bankLib.getBanks()
+
+      return res.json({result: 'ok', ret})
+    } catch (e) {
+      return res.json({result: 'error', msg: e.message})
+    }
+  })
+
+  /**
+   * GET /api/bank
+   *
+   * 取得單一銀行詳細資料API
+   */
+  app.get('/api/bank', async (req, res) => {
+    const code = req.query.code
+
+    try {
+      const ret = await bankLib.getBank(code)
+      ret.accounts = await bankLib.getBankAccount(code)
 
       return res.json({result: 'ok', ret})
     } catch (e) {
